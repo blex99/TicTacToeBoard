0ac16384bb7718ac708d4beb29931a04c4ed0c1d
blex99 <alex.tchap99@gmail.com>
2020-12-02 12:15:27 -0800

    ttt implementation and testing

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -2,54 +2,131 @@
 /**
  * Class for representing a 3x3 Tic-Tac-Toe game board, using the Piece enum
  * to represent the spaces on the board.
-**/
+ **/
 
 //Constructor sets an empty board and specifies it is X's turn first
 TicTacToeBoard::TicTacToeBoard()
 {
-  turn = X;
-  for(int i=0; i<BOARDSIZE; i++)
-    for(int j=0; j<BOARDSIZE; j++)
-      board[i][j] = Blank;
+	turn = X;
+	for(int i=0; i<BOARDSIZE; i++)
+		for(int j=0; j<BOARDSIZE; j++)
+			board[i][j] = Blank;
 }
 
 /**
  * Switches turn member variable to represent whether it's X's or O's turn
  * and returns whose turn it is
-**/
+ **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+	if (turn == X) turn = O;
+	else turn = X;
+
+	return turn;
 }
 
 /**
  * Places the piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. placePiece does 
+ * piece is placed, and toggles which Piece's turn it is.
+ * placePiece does 
  * NOT allow to place a piece in a location where there is already a piece.
  * In that case, placePiece just returns what is already at that location. 
  * Out of bounds coordinates return the Piece Invalid value. When the game
  * is over, no more pieces can be placed so attempting to place a piece
  * should neither change the board nor change whose turn it is.
-**/ 
+ **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+	if (row > 2 || column > 2 || row < 0 || column < 0)
+		return Invalid;
+
+	if (board[row][column] == Blank)
+	{
+		board[row][column] = turn;
+		toggleTurn();
+		return board[row][column];
+	}
+
+	return board[row][column];
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+	if (row > 2 || column > 2 || row < 0 || column < 0)
+		return Invalid;
+
+	return board[row][column];
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won.
-**/
+ **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+	// rows
+	for (int i=0; i<BOARDSIZE; i++) 
+	{ 
+		if (board[i][0] == board[i][1] && 
+				board[i][1] == board[i][2] &&  
+				board[i][0] != Blank) 
+		{
+			return board[i][0]; 
+		}
+	} 
+
+	// columns
+	for (int i=0; i<BOARDSIZE; i++) 
+	{ 
+		if (board[0][i] == board[1][i] && 
+				board[1][i] == board[2][i] &&  
+				board[0][i] != Blank) 
+		{
+			return board[0][i];
+		}
+	} 
+
+	// diagonals
+	if (board[0][0] == board[1][1] && 
+			board[1][1] == board[2][2] &&  
+			board[0][0] != Blank)
+	{
+		return board[0][0];
+	}
+
+	if (board[0][2] == board[1][1] && 
+			board[1][1] == board[2][0] && 
+			board[0][2] != Blank) 
+	{
+		return board[0][2];
+	}
+
+	// check if the board is NOT full
+	for (int i=0; i < BOARDSIZE; i++)
+	{
+		for (int j=0; j < BOARDSIZE; j++)
+		{
+			if (board[i][j] == Blank)
+				return Invalid; // game isn't over
+		}
+	}
+
+	return Blank; // tie
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -1,10 +1,10 @@
 /**
  * Unit Tests for TicTacToeBoard
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+
 class TicTacToeBoardTest : public ::testing::Test
 {
 	protected:
@@ -14,9 +14,106 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-/* EXAMPLE TEST FORMAT
-TEST(TicTacToeBoardTest, unitTestName)
+TEST(TicTacToeBoardTest, toggleGetPlacePieceTest)
 {
-	ASSERT_TRUE(true);
+	TicTacToeBoard ttt;
+	
+	// the turn has been switched to O's
+	ASSERT_TRUE(O == ttt.toggleTurn());
+
+	// place piece
+	ASSERT_TRUE(ttt.placePiece(1, 1) == O); // now X's turn
+
+	ASSERT_TRUE(ttt.placePiece(1, 1) == O); // there's an O already there!
+
+	ASSERT_TRUE(ttt.placePiece(-1, 0) == Invalid); // out of range
+	ASSERT_TRUE(ttt.placePiece(3, 0) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(0, -1) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(0, 3) == Invalid);
+
+	ASSERT_TRUE(ttt.placePiece(1, 2) == X); // now O's turn
+
+	// get piece
+	ASSERT_TRUE(ttt.getPiece(1, 2) == X); 
+	ASSERT_TRUE(ttt.getPiece(1, 1) == O); 
+	ASSERT_TRUE(ttt.getPiece(0, 0) == Blank); 
+	ASSERT_TRUE(ttt.getPiece(-1, 0) == Invalid); // out of range
 }
-*/
+
+
+TEST(TicTacToeBoardTest, winnerTest)
+{
+	TicTacToeBoard ttt[3];
+
+	// testing game not over
+	// X   
+	// X O 
+	//   O 
+	ttt[0].placePiece(0, 0);
+	ttt[0].placePiece(1, 1);
+	ttt[0].placePiece(1, 0);
+	ttt[0].placePiece(2, 1);
+	ASSERT_TRUE(ttt[0].getWinner() == Invalid);
+
+	// testing col win X
+	// X  
+	// X O 
+	// X O
+	ttt[0].placePiece(0, 0);
+	ttt[0].placePiece(1, 1);
+	ttt[0].placePiece(1, 0);
+	ttt[0].placePiece(2, 1);
+	ttt[0].placePiece(2, 0);
+	ASSERT_TRUE(ttt[0].getWinner() == X);
+
+	// testing row win O
+	// O O O
+	// X X
+	// X
+	ttt[1].placePiece(1, 1);
+	ttt[1].placePiece(0, 0);
+	ttt[1].placePiece(1, 0);
+	ttt[1].placePiece(0, 1);
+	ttt[1].placePiece(2, 0);
+	ttt[1].placePiece(0, 2);
+	ASSERT_TRUE(ttt[1].getWinner() == O);
+
+	// testing board is filled and there's a tie
+	// x o x
+	// o o x
+	// x x o
+	ttt[2].placePiece(0, 0);
+	ttt[2].placePiece(0, 1);
+
+	ttt[2].placePiece(0, 2);
+	ttt[2].placePiece(1, 0);
+
+	ttt[2].placePiece(1, 2);
+	ttt[2].placePiece(1, 1);
+
+	ttt[2].placePiece(2, 0);
+	ttt[2].placePiece(2, 2);
+
+	ttt[2].placePiece(2, 1);
+
+	ASSERT_TRUE(ttt[2].getWinner() == Blank);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
